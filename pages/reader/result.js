adc9ae4a20b7711089283b344a74f6c1d647ba89
zhangjh <njhxzhangjihong@126.com>
2025-01-08 00:45:36 +0800

    下拉刷新&下载

--- pages/reader/result.js
+++ pages/reader/result.js
@@ -3,7 +3,8 @@ const common = require("../common/index");
 Page({
   data: {
     searchQuery: '',   // 搜索关键词
-    bookList: [],      // 当前页的书籍数据列表   
+    bookList: [],      // 当前页的书籍数据列表  
+    hasMore: true, 
     page: 1,
     limit: 5
   },
@@ -14,16 +15,38 @@ Page({
     this.setData({ searchQuery: query });
 
     // 开始搜索
-    this.searchBooks(query);
+    this.searchBooks();
   },
 
-  searchBooks(query) {
+  onReachBottom() {
+    if(!this.data.hasMore) {
+      wx.showToast({
+        title: '没有更多了...'
+      });
+      return;
+    }
+    this.setData({
+      page: this.data.page + 1
+    });
+    this.searchBooks();
+  },
+
+  searchBooks() {
     common.wxRequest({
-      url: `/books/search?page=${this.data.page}&limit=${this.data.limit}&keyword=` + encodeURIComponent(query),
+      url: `/books/search?page=${this.data.page}&limit=${this.data.limit}&keyword=` + encodeURIComponent(this.data.searchQuery),
       cb: res => {
-        console.log(res);
+        if(!res || !res.length) {
+          this.setData({
+            hasMore: false
+          });
+          return;
+        }
+        let bookList = this.data.bookList;
+        res.map(item => {
+          bookList.push(item);
+        });
         this.setData({
-          bookList: res
+          bookList
         });
       },
       failCb: err => {
@@ -32,11 +55,11 @@ Page({
     });
   },
   onDownload(e) {
-    console.log(e);
     const bookid = e.currentTarget.dataset.id;
     const hash = e.currentTarget.dataset.hash;
-    console.log("bookid:" + bookid);
-    console.log("hash:" + hash);
+    wx.showLoading({
+      title: '文件下载中',
+    });
     common.wxRequest({
       url: '/books/download',
       method: 'POST',
@@ -48,22 +71,37 @@ Page({
         wx.downloadFile({
           url: res,
           success: ret => {
-            console.log(ret.tempFilePath);
+            wx.hideLoading();
             const fileManager = wx.getFileSystemManager();
-            fileManager.saveFile(ret.tempFilePath);
-            wx.showToast({
-              title: '文件存储在:' + ret.tempFilePath,
+            fileManager.saveFile({
+              tempFilePath: ret.tempFilePath,
+              success: saveRes => {
+                console.log(saveRes.savedFilePath);
+                wx.showToast({
+                  title: '下载成功',
+                });
+              },
+              fail: saveErr => {
+                console.error(saveErr);
+                wx.hideLoading();
+                wx.showToast({
+                  title: '文件存储失败',
+                  icon: 'error'
+                });
+              }
             });
           },
           fail: err => {
+            console.log(err);
             wx.showToast({
-              title: '文件存储失败',
+              title: '文件下载失败',
               icon: 'error'
             });
           }
         })       
       },
       failCb: err => {
+        wx.hideLoading();
         console.log(err);
       }
     });

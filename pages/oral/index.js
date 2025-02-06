3c75c594d6633f6d04b3bdf755e0386c3c9c29df
zhangjh <njhxzhangjihong@126.com>
2025-02-07 02:04:21 +0800

    实现oral录音&播放

--- pages/oral/index.js
+++ pages/oral/index.js
@@ -2,6 +2,7 @@
 const common = require("../common/index");
 const fs = wx.getFileSystemManager();
 const canto = require("../canto/canto");
+const chat = require("../common/chat");
 const wxAudio = wx.createInnerAudioContext({});
 
 Page({
@@ -14,9 +15,15 @@ Page({
     topics: [
     ],
     playing: false,
+    /**
+     * [{ role: 'model', content: '', type: 'base64' }]
+     */
     chatContent: null,
     playContent: null,
     ttsAudio: null,
+    recording: false,
+    context: [],
+    tmpFiles: []
   },
 
   /**
@@ -26,6 +33,20 @@ Page({
     common.setTabBarTitle('英语开口说');
     // 初始化获取话题
     this.getTopics();
+  },
+
+  onUnload() {
+    // 卸载临时文件
+    const tmpFiles = this.data.tmpFiles;
+    for (const tmpFile of tmpFiles) {
+      console.log(tmpFile);
+      fs.unlink({
+        filePath: tmpFile,
+        success: () => {
+          console.log('临时文件已删除');
+        },
+      });
+    }
   },
 
   /**
@@ -62,6 +83,7 @@ Page({
     const topic = e.target.dataset.item;
     this.closeTopicDialog();
     // 选中主题后主动开启对话
+    this.buildContext('user', 'hello');
     this.conversation({
       topic, 
       talkMode: 'tutor', 
@@ -71,32 +93,39 @@ Page({
     });
   },
   conversation(data) {
-    let tutorContent = '';
+    let content = '';
     const requestTask = common.wxRequest({
       url: '/wxChat/oral',
       method: 'POST',
       data: {
         topic: data.topic,
         userContent: data.content,
+        audio: data.audio,
         talkMode: data.talkMode,
         context: data.context
       },
       enableChunked: true,
+      cb: res => {
+        console.log("task end");
+        // play audio
+        this.setData({
+          ttsAudio: null
+        });
+        this.playContent(this.data.playContent);
+        this.buildChatContent('model', this.data.playContent);
+        this.buildContext('model', this.data.playContent);
+      }
     });
     requestTask.onChunkReceived((response) => {
       try {
         const chunk = decodeURIComponent(escape(String.fromCharCode.apply(null, new Uint8Array(response.data))));
         // 动态设值
-        tutorContent += chunk;
-        if(!this.data.chatContent) {
-          this.data.chatContent = [{
-            teacher: tutorContent
-          }];
-        } else {
-          this.data.chatContent[0].teacher = tutorContent;
-        }
+        console.log("chunk");
+        content += chunk;
+        this.buildChatContent('model', content);
         this.setData({
-          chatContent: this.data.chatContent
+          chatContent: this.data.chatContent,
+          playContent: content
         });
       } catch (e) {
         console.error('Process chunk failed:', e);
@@ -115,7 +144,11 @@ Page({
     });
   },
   playContent(e) {
-    const content = e.target.dataset.content;
+    // 默认取状态，如果dataset有值则为主动点击播放
+    let content = this.data.playContent;
+    if(e.target) {
+      content = e.target.dataset.content;
+    }
     console.log(content);
     this.setData({
       playing: true
@@ -127,7 +160,7 @@ Page({
       wx.request({
         url: canto.cantoDomain + "/canto/voice/playByOral?text=" + content 
           + "&voiceName=en-US-EvelynMultilingualNeural"
-          + "&lang=en-GB"
+          + "&lang=en-US"
           + "&rate=medium",
         responseType: 'arraybuffer',
         success: res => {
@@ -143,8 +176,11 @@ Page({
                 this.playAudio(target);
               }
             });
+            let tmpFiles = this.data.tmpFiles;
+            tmpFiles.push(target);
             this.setData({
-              ttsAudio: target
+              ttsAudio: target,
+              tmpFiles
             });
           }
         }
@@ -153,6 +189,93 @@ Page({
   },
   onRecord() {
     console.log("record");
+    this.setData({
+      recording: true
+    });
     common.requestRecordPermission();
   },
+  cancelRecord() {
+    this.setData({
+      recording: false
+    });
+  },
+  buildChatContent(role, content, type) {
+    if(!this.data.chatContent) {
+      this.data.chatContent = [];
+    }
+    const len = this.data.chatContent.length;
+    let chatContentItem = this.data.chatContent[len - 1];
+    // 同角色累加信息
+    if(chatContentItem && chatContentItem.role === role) {
+      chatContentItem.content = content;
+    } else {
+      this.data.chatContent.push({
+        role, content, type
+      });
+    }
+    this.setData({
+      chatContent: this.data.chatContent
+    });
+  },
+  buildContext(role, content, audio) {
+    /**
+      role: 'user',
+        parts: [{
+          text: 'Hello'
+        }]
+      }
+     */
+    let context = this.data.context;
+    if(content) {
+      context.push({
+        role,
+        parts: [{
+          text: content
+        }]
+      });
+    }
+    if(audio) {
+      // todo: 增加音频上下文
+    }
+  },
+  send(e) {
+    console.log("send");
+    this.setData({
+      recording: false
+    });
+    // 获取录制的音频
+    const recorderManager = wx.getRecorderManager();
+    recorderManager.onStop((res) => {
+      console.log('停止录音', res);
+      const { tempFilePath } = res;
+      // 获取文件管理器
+      const fileSystemManager = wx.getFileSystemManager();
+      // 读取文件并转为 Base64
+      fileSystemManager.readFile({
+        filePath: tempFilePath, // 录音文件的临时路径
+        encoding: 'base64', // 指定编码为 base64
+        success: (result) => {
+          const base64Data = result.data; // 获取 Base64 数据
+          console.log('Base64 数据:', base64Data);
+          // 你可以在这里将 Base64 数据上传到服务器或进行其他处理
+          this.buildChatContent('user', tempFilePath, 'base64');
+          this.buildContext('user', null, base64Data);
+          this.conversation({
+            talkMode: 'tutor', 
+            audio: [{
+              mimeType: 'audio/wav',
+              base64: base64Data
+            }],
+            context: {
+              messages: this.data.context
+            }
+          });
+        },
+        fail: (err) => {
+          console.error('读取文件失败:', err);
+        },
+      });
+    });
+    recorderManager.stop();
+  }
 })

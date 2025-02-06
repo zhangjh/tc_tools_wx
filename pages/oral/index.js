0df74b935fc81db47f7ddb6dcee2ee2e8caad4f5
zhangjh <njhxzhangjihong@126.com>
2025-02-06 18:57:35 +0800

    英语开口说初步搭建

--- pages/oral/index.js
+++ pages/oral/index.js
@@ -1,60 +1,31 @@
 // pages/oral/index.js
+const common = require("../common/index");
+const fs = wx.getFileSystemManager();
+const canto = require("../canto/canto");
+const wxAudio = wx.createInnerAudioContext({});
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    showTopicDialog: false,
+    topics: [
+    ],
+    playing: false,
+    chatContent: null,
+    playContent: null,
+    ttsAudio: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
+    common.setTabBarTitle('英语开口说');
+    // 初始化获取话题
+    this.getTopics();
   },
 
   /**
@@ -62,5 +33,123 @@ Page({
    */
   onShareAppMessage() {
 
-  }
+  },
+  getTopics() {
+    wx.showLoading({
+      title: 'Topics Loading...',
+    });
+    common.wxRequest({
+      url: '/wxChat/oral/topics',
+      cb: res => {
+        wx.hideLoading();
+        this.setData({
+          showTopicDialog: true,
+          topics: JSON.parse(res)
+        });
+      }
+    });
+  },
+  refreshTopics() {
+    console.log("refresh");
+    this.getTopics();
+  },
+  closeTopicDialog() {
+    this.setData({
+      showTopicDialog: false
+    });
+  },
+  selectTopic(e) {
+    const topic = e.target.dataset.item;
+    this.closeTopicDialog();
+    // 选中主题后主动开启对话
+    this.conversation({
+      topic, 
+      talkMode: 'tutor', 
+      content: 'hello',
+      audio: null,
+      context: null
+    });
+  },
+  conversation(data) {
+    let tutorContent = '';
+    const requestTask = common.wxRequest({
+      url: '/wxChat/oral',
+      method: 'POST',
+      data: {
+        topic: data.topic,
+        userContent: data.content,
+        talkMode: data.talkMode,
+        context: data.context
+      },
+      enableChunked: true,
+    });
+    requestTask.onChunkReceived((response) => {
+      try {
+        const chunk = decodeURIComponent(escape(String.fromCharCode.apply(null, new Uint8Array(response.data))));
+        // 动态设值
+        tutorContent += chunk;
+        if(!this.data.chatContent) {
+          this.data.chatContent = [{
+            teacher: tutorContent
+          }];
+        } else {
+          this.data.chatContent[0].teacher = tutorContent;
+        }
+        this.setData({
+          chatContent: this.data.chatContent
+        });
+      } catch (e) {
+        console.error('Process chunk failed:', e);
+      }
+    });
+  },
+  playAudio(target) {
+    wxAudio.src = target;
+    wxAudio.play();
+    // 添加音频播放结束的监听器
+    wxAudio.onEnded(() => {
+      // this.hideVoicePrint();
+      this.setData({
+        playing: false
+      });
+    });
+  },
+  playContent(e) {
+    const content = e.target.dataset.content;
+    console.log(content);
+    this.setData({
+      playing: true
+    });
+    if(this.data.playContent === content && this.data.ttsAudio) {
+      this.playAudio(this.data.ttsAudio);
+    } else {
+      const target = `${wx.env.USER_DATA_PATH}/${Date.now()}.wav`;
+      wx.request({
+        url: canto.cantoDomain + "/canto/voice/play?text=" + content + "&voiceRole=" + 0,
+        responseType: 'arraybuffer',
+        success: res => {
+          if(res.statusCode === 200) {
+            fs.writeFile({
+              filePath: target,
+              data: res.data,
+              encoding: 'binary',
+              success: res => {
+                this.setData({
+                  playContent: content
+                });
+                this.playAudio(target);
+              }
+            });
+            this.setData({
+              ttsAudio: target
+            });
+          }
+        }
+      });
+    }
+  },
+  onRecord() {
+    console.log("record");
+    common.requestRecordPermission();
+  },
 })
